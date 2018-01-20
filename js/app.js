bb29e2ac2a363be2144fac65fa1f11cdb7171c13
andrew-cloud <cloud.andr@gmail.com>
2018-01-20 12:56:47 -0500

    added comments
    Signed-off-by: andrew-cloud <cloud.andr@gmail.com>

--- js/app.js
+++ js/app.js
@@ -1,24 +1,27 @@
 $(document).foundation();
 
 
-
+// inputted words
 let words = "";
+// keeps track of word count
 let count = "0"
 
-// document.getElementById("words-input").value;
-
+// gets inputted words
 function refreshWords() {
 	words = document.getElementById("words-input").value;
 }
 
+// refreshed word count
 function refreshCount() {
 	count = wordCount(words);
 }
 
+// determines word count
 function wordCount(str) {
 	str.split(" ").length;
 }
 
+// event listeners to updated values
 document.addEventListener("keyup", refreshWords);
 document.addEventListener("keyup", refreshCount);
 
