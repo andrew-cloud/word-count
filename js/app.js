eda9d2eab68529844707203764341b0eec49fe23
andrew-cloud <cloud.andr@gmail.com>
2018-01-20 13:16:24 -0500

    comments
    Signed-off-by: andrew-cloud <cloud.andr@gmail.com>

--- js/app.js
+++ js/app.js
@@ -1,23 +1,27 @@
 $(document).foundation();
 
 
-
+// inputted words
 let words = "";
+// word count
 let count = "0";
 
-
+// gets inputted words form text area
 function refreshWords() {
 	words = document.getElementById("words-input").value;
 }
 
+// refreshes word count
 function refreshCount() {
 	count = wordCount(words);
 }
 
+// word count
 function wordCount(str) {
 	return str.split(" ").length;
 }
 
+// refreshes values after input
 document.addEventListener("keyup", refreshWords);
 document.addEventListener("keyup", refreshCount);
 
