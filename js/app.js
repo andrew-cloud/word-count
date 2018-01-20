69fee1b4d696de00a8286b6843f98165b4056947
andrew-cloud <cloud.andr@gmail.com>
2018-01-20 12:52:42 -0500

    registers inputted words and word count
    Signed-off-by: andrew-cloud <cloud.andr@gmail.com>

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
 $(document).foundation();
+
+
+
+let words = "";
+let count = "0"
+
+// document.getElementById("words-input").value;
+
+function refreshWords() {
+	words = document.getElementById("words-input").value;
+}
+
+function refreshCount() {
+	count = wordCount(words);
+}
+
+function wordCount(str) {
+	str.split(" ").length;
+}
+
+document.addEventListener("keyup", refreshWords);
+document.addEventListener("keyup", refreshCount);
+
+console.log(count);
