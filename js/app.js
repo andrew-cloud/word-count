1c42ca01db5e0313abd6ec3933b336dc13131601
andrew-cloud <cloud.andr@gmail.com>
2018-01-20 13:13:00 -0500

    fixed word count
    Signed-off-by: andrew-cloud <cloud.andr@gmail.com>

--- js/app.js
+++ js/app.js
@@ -1,27 +1,23 @@
 $(document).foundation();
 
 
-// inputted words
-let words = "";
-// keeps track of word count
-let count = "0"
 
-// gets inputted words
+let words = "";
+let count = "0";
+
+
 function refreshWords() {
 	words = document.getElementById("words-input").value;
 }
 
-// refreshed word count
 function refreshCount() {
 	count = wordCount(words);
 }
 
-// determines word count
 function wordCount(str) {
-	str.split(" ").length;
+	return str.split(" ").length;
 }
 
-// event listeners to updated values
 document.addEventListener("keyup", refreshWords);
 document.addEventListener("keyup", refreshCount);
 
--- js/min/app-min.js
+++ js/min/app-min.js
@@ -1,2 +1,2 @@
-"use strict";function refreshWords(){words=document.getElementById("words-input").value}function refreshCount(){count=wordCount(words)}function wordCount(e){e.split(" ").length}$(document).foundation();var words="",count="0";document.addEventListener("keyup",refreshWords),document.addEventListener("keyup",refreshCount),console.log(count);
+"use strict";function refreshWords(){words=document.getElementById("words-input").value}function refreshCount(){count=wordCount(words)}function wordCount(e){return e.split(" ").length}$(document).foundation();var words="",count="0";document.addEventListener("keyup",refreshWords),document.addEventListener("keyup",refreshCount),console.log(count);
 //# sourceMappingURL=./app-min.js.map
