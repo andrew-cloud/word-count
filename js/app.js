473ee9bc48af7f79fde08e8f7ce5fb0d05b70b36
andrew-cloud <cloud.andr@gmail.com>
2018-01-20 13:58:37 -0500

    word count improvements
    Signed-off-by: andrew-cloud <cloud.andr@gmail.com>

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@ $(document).foundation();
 let words = "";
 // word count
 let count = "0";
+// unit-counter
+let unit_counter = document.getElementById("unit-counter").innerHTML;
 
 // gets inputted words form text area
 function refreshWords() {
@@ -17,12 +19,21 @@ function refreshCount() {
 }
 
 // word count
-function wordCount(str) {
-	return str.split(" ").length;
+function wordCount(s) {
+	s = s.replace(/(^\s*)|(\s*$)/gi,""); //exclude  start and end white-space
+    s = s.replace(/[ ]{2,}/gi," "); // 2 or more spaces to 1
+    s = s.replace(/\n /,"\n"); // exclude newline with a start spacing
+    return s.split(' ').length; 
+}
+
+// refresh displayed count
+function refreshCounter() {
+	document.getElementById("unit-counter").innerHTML = `${count}/250`;
 }
 
 // refreshes values after input
 document.addEventListener("keyup", refreshWords);
 document.addEventListener("keyup", refreshCount);
+document.addEventListener("keyup", refreshCounter);
 
 console.log(count);
