1d3eccc4cbfc23f2ec22ff4eb97bb8434931e02e
andrew-cloud <cloud.andr@gmail.com>
2018-04-22 14:39:08 -0400

    got the search to work
    Signed-off-by: andrew-cloud <cloud.andr@gmail.com>

--- js/all_entries.js
+++ js/all_entries.js
@@ -4,19 +4,19 @@ $(document).foundation();
 let all_entries = document.getElementsByClassName("entry");
 let search_input = document.getElementById("search-input");
 let container = document.getElementById("all-entries-inner-container");
-let search_term = search_input.value;
+let search_term = search_input.value.toLowerCase();
 // console.log(all_entries[0].innerText);
 
 // refreshes search term
 function refreshSearchTerm() {
-    search_term = search_input.value;
+    search_term = search_input.value.toLowerCase();
 }
 
 // loops through all entiries on page
 function searchEntry() {
     var i;
     for (i = 0; i < all_entries.length; i++) {
-        if (all_entries[i].innerText.includes(search_term)) {
+        if (all_entries[i].innerText.toLowerCase().includes(search_term)) {
         	console.log(all_entries[i].innerText)
             all_entries[i].classList.remove("hide");
             all_entries[i].parentElement.classList.remove("hide");
